ae10da5554a530923b21060cc769df57860c618e
Antonio Barba <antonio.barba.external@enel.com>
2024-12-13 01:25:00 +0100

    fix USE_GPU define and better python wrapper

--- src/main.py
+++ src/main.py
@@ -8,27 +8,27 @@ y = 4096
 
 # create a buffer
 imagebuffer1 = w.makebuffer(x, y)
+mandel_cpu = w.mandel(use_gpu = False)
+mandel_gpu = w.mandel(use_gpu = True)
 
 print("benchmarking CPU")
 
 start = time.time_ns()
 for i in range(5):
-    w.mandelbrot_cpu(imagebuffer1, x, y)
+    mandel_cpu.run(imagebuffer1, x, y)
 elapsed = (time.time_ns() - start) / 5000000
 print(f"average over 5 runs {elapsed}ms")
 
 PIL.Image.frombuffer(size=(x,y), data=imagebuffer1, mode="RGBA", decoder_name="raw").show()
 
 imagebuffer2 = w.makebuffer(x, y)
-kernel = w.mandelbrot_create_gpu_kernel()
 
 print("benchmarking GPU")
 start = time.time_ns()
 for i in range(5):
-    w.mandelbrot_gpu(kernel, imagebuffer2, x, y)
+    mandel_gpu.run(imagebuffer2, x, y)
 elapsed = (time.time_ns() - start) / 5000000
 print(f"average over 5 runs {elapsed}ms")
 
-w.mandelbrot_free_gpu_kernel(kernel)
 PIL.Image.frombuffer(size=(x,y), data=imagebuffer2, mode="RGBX").show()
 
